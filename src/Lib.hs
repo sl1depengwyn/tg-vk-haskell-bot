2c0776430e4ae653e89823dbfabb947c74f34f10
Max <max.pengwyn@gmail.com>
2022-01-27 23:39:47 +0300

    implemented getUpdates and runBot
    bot now able to reply to single text message

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,24 +2,29 @@ module Lib
     ( someFunc
     , sendMessage
     , getUpdates
+    , runBot
     ) where
 
 
 
 import qualified Control.Monad.IO.Class     as MIO
+import           Data.Aeson
 import qualified Data.ByteString            as B
 import qualified Data.ByteString.Char8      as BC
 import qualified Data.ByteString.Lazy       as L
 import qualified Data.ByteString.Lazy.Char8 as LC
 import qualified Data.Text                  as T
 import qualified Data.Text.Encoding         as TE
+import qualified GHC.Generics as G
 import           Network.HTTP.Simple
+
+
+
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
 
-type Message = T.Text
 type ChatID = BC.ByteString
 
 buildRequest :: BC.ByteString -> BC.ByteString -> Query -> Request
@@ -30,7 +35,8 @@ buildRequest host path query = setRequestHost host
                              $ setRequestPort 443
                                defaultRequest
 
--- >>> buildRequest host (mconcat ["/bot", apiKey, "/sendMessage"]) [("chat_id", Just "385474228"), ("text", Just "s")]
+-- >>> buildRequest host (mconcat ["/bot", apiKey, "/sendMessage"]) [("chat_id", Nothing), ("text", Just "s")]
+
 
 
 
@@ -38,17 +44,61 @@ host :: BC.ByteString
 host = "api.telegram.org"
 
 apiKey :: BC.ByteString
-apiKey = "1438339694:AAG096Iqn-PGcd0zlBBHJ_7MwebucYdfF6M"
+apiKey = "<Your API Key>"
 
 
 sendMessage :: MIO.MonadIO m => ChatID -> Message -> m (Response BC.ByteString)
 sendMessage chatId message = httpBS $ buildRequest host path query
     where path = mconcat ["/bot", apiKey, "/sendMessage"]
-          query = [("chat_id", Just chatId), ("text", Just (TE.encodeUtf8 message))]
+          messageText = text message
+          query = [("chat_id", Just chatId), ("text", Just (TE.encodeUtf8 messageText))]
 
 
 
-getUpdates :: MIO.MonadIO m => m (Response BC.ByteString)
-getUpdates = httpBS $ buildRequest host path query
-    where path = mconcat ["/bot", apiKey, "/getUpdates"]
-          query = [("timeout", Just "25")]
+data User = User { id    :: Int
+                 , isBot :: Bool } deriving (Show, G.Generic)
+instance FromJSON User where
+    parseJSON (Object o) = User <$> o .: "id"
+                                <*> o .: "is_bot"
+
+data Message = Message { from :: User
+                       , text :: T.Text } deriving (Show, G.Generic)
+instance FromJSON Message
+
+data Update = Update { updateId :: Int
+                     , message  :: Message } deriving (Show)
+
+instance FromJSON Update where
+    parseJSON (Object o) = Update <$> o .: "update_id"
+                                  <*> o .: "message"
+
+data UpdatesResponse = UpdatesResponse { result :: [Update] } deriving (Show, G.Generic)
+instance FromJSON UpdatesResponse
+
+getUpdates :: (MIO.MonadIO m) => Maybe Int -> m (Either String [Update])
+getUpdates offset = do
+        let path = mconcat ["/bot", apiKey, "/getUpdates"]
+        let query = [("timeout", Just "25"), ("offset", BC.pack . show <$> offset)]
+        response <- httpBS $ buildRequest host path query
+        let responseBody = getResponseBody response
+        let responseJson = eitherDecode ((LC.fromChunks . return) responseBody) :: Either String UpdatesResponse
+        let updates = result <$> responseJson
+        return updates
+
+
+replyMessage update = sendMessage senderId (message update)
+    where (User sndrId _) = (from . message) update
+          senderId = (BC.pack . show) sndrId
+         
+
+
+
+runBot :: IO ()
+runBot = do
+    updatesM <- getUpdates Nothing
+    let (Right updates) = updatesM
+    mapM_ replyMessage updates
+
+
+
+
