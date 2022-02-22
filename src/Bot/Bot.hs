ba65a7c0ac46194471f94116ba4ad393055588a4
Max <max.pengwyn@gmail.com>
2022-02-23 00:28:10 +0300

    way closer to handle pattern

--- src/Bot/Bot.hs
+++ src/Bot/Bot.hs
@@ -1,8 +1,10 @@
 module Bot.Bot where
 
 import qualified Bot.Logger                 as Logger
-import           Control.Monad              (MonadPlus (mzero), replicateM)
+import           Control.Monad              (MonadPlus (mzero), replicateM,
+                                             void)
 import qualified Control.Monad.IO.Class     as MIO
+import           Control.Monad.State
 import qualified Data.Aeson.Extended        as A
 import qualified Data.ByteString            as B
 import qualified Data.ByteString.Char8      as BC
@@ -16,7 +18,6 @@ import qualified Data.Text.Encoding         as TE
 import qualified Data.Yaml                  as Yaml
 import qualified GHC.Generics               as G
 import           Network.HTTP.Simple
-import Control.Monad.State
 
 data Config =
   Config
@@ -31,18 +32,22 @@ data Config =
 instance A.FromJSON Config where
   parseJSON = A.genericParseJSON A.customOptions
 
+type MapUserToRepeat = Map Int Int
+
 data Handle =
   Handle
     { hConfig :: Config
     , hLogger :: Logger.Handle
     }
 
+data BotState =
+  BotState
+    { sUsers  :: MapUserToRepeat
+    , sOffset :: Maybe Int
+    }
+
 withHandle :: Config -> Logger.Handle -> (Handle -> IO ()) -> IO ()
 withHandle conf lHandle f = f $ Handle {hConfig = conf, hLogger = lHandle}
-
-type MapUserToRepeat = Map Int Int
-
-type BotM = State MapUserToRepeat
 
 host :: BC.ByteString
 host = "api.telegram.org"
@@ -51,11 +56,6 @@ buildRequest :: BC.ByteString -> BC.ByteString -> Query -> Request
 buildRequest host path query =
   setRequestHost host $
   setRequestQueryString query $ setRequestPath path $ setRequestSecure True $ setRequestPort 443 defaultRequest
-
-replyKeyboard :: InlineKeyboard
-replyKeyboard =
-  InlineKeyboard
-    [[SimpleButton "1" "1", SimpleButton "2" "2"], [SimpleButton "3" "3", SimpleButton "4" "4", SimpleButton "5" "5"]]
 
 sendMessage :: T.Text -> Int -> T.Text -> Query -> IO BC.ByteString
 sendMessage apiKey userId method query = getResponseBody <$> httpBS (buildRequest host (TE.encodeUtf8 path) finalQuery)
@@ -80,6 +80,11 @@ sendSticker apiKey fileId userId = sendMessage apiKey userId "/sendSticker" quer
   where
     query = [("sticker", Just (TE.encodeUtf8 fileId))]
 
+replyKeyboard :: InlineKeyboard
+replyKeyboard =
+  InlineKeyboard
+    [[SimpleButton "1" "1", SimpleButton "2" "2"], [SimpleButton "3" "3", SimpleButton "4" "4", SimpleButton "5" "5"]]
+
 newtype User =
   User
     { uId :: Int
@@ -95,7 +100,8 @@ newtype Sticker =
     }
   deriving (Show, G.Generic)
 
-instance A.FromJSON Sticker
+instance A.FromJSON Sticker where
+  parseJSON = A.genericParseJSON A.customOptions
 
 newtype InlineKeyboard =
   InlineKeyboard
@@ -164,7 +170,7 @@ newtype UpdatesResponse =
 
 instance A.FromJSON UpdatesResponse
 
-getUpdates :: Show a => T.Text -> Maybe a -> IO (Either String [Update])
+getUpdates :: T.Text -> Maybe Int -> IO (Either String [Update])
 getUpdates apiKey offset = do
   let path = mconcat ["/bot", apiKey, "/getUpdates"]
   let query = [("timeout", Just "25"), ("offset", BC.pack . show <$> offset)]
@@ -184,17 +190,6 @@ replyMessage usersDB noRepetitions userId f =
   case Map.lookup userId usersDB of
     (Just noReps) -> mconcat <$> replicateM noReps (f userId)
     Nothing       -> mconcat <$> replicateM noRepetitions (f userId)
-
-processMessage :: Config -> MapUserToRepeat -> Message -> IO BC.ByteString
-processMessage (Config api helpMsg repeatMsg _ noRepetitions) usersDB msg@(TextMessage (User userId) text) =
-  case text of
-    "/help" -> sendTextMessage api helpMsg userId
-    "/repeat" -> sendKeyboardMessage api userId repeatMsg
-    _ -> replyMessage usersDB noRepetitions userId (sendTextMessage api text)
-processMessage (Config api _ _ _ noRepetitions) usersDB (StickerMessage (User userId) (Sticker fileId)) =
-  replyMessage usersDB noRepetitions userId (sendSticker api fileId)
-processMessage (Config api _ _ failMsg _) usersDB (UnsupportedMessage (User userId)) =
-  sendTextMessage api failMsg userId
 
 processCallback :: MapUserToRepeat -> CallbackQuery -> MapUserToRepeat
 processCallback usersDB (CallbackQuery (User usrId) amount) = Map.insert usrId (read amount) usersDB
@@ -225,26 +220,112 @@ longPolling config usersDB offset = do
   mapM_ (maybe (BC.putStrLn "map updated") BC.putStrLn) responseBodies
   longPolling config upToDateDB lastUpdateId
 
-runBot :: Handle -> IO ()
-runBot handle = longPolling (hConfig handle) Map.empty Nothing
+-- runBot :: Handle -> IO ()
+-- runBot handle = longPolling (hConfig handle) Map.empty Nothing
+runBot botH = evalStateT (longPolling' botH) (BotState {sUsers = Map.empty, sOffset = Nothing})
 
+processMessage :: Config -> MapUserToRepeat -> Message -> IO BC.ByteString
+processMessage (Config api helpMsg repeatMsg _ noRepetitions) usersDB msg@(TextMessage (User userId) text) =
+  case text of
+    "/help" -> sendTextMessage api helpMsg userId
+    "/repeat" -> sendKeyboardMessage api userId repeatMsg
+    _ -> replyMessage usersDB noRepetitions userId (sendTextMessage api text)
+processMessage (Config api _ _ _ noRepetitions) usersDB (StickerMessage (User userId) (Sticker fileId)) =
+  replyMessage usersDB noRepetitions userId (sendSticker api fileId)
+processMessage (Config api _ _ failMsg _) usersDB (UnsupportedMessage (User userId)) =
+  sendTextMessage api failMsg userId
 
+type ReceiverId = Int
 
-longPolling' :: Handle -> BotM () -> IO ()
-longPolling' botH usersDB = do
-  let (Config apiKey _ _ _ _) = config
-  eitherUpdates <- getUpdates apiKey offset
-  let (Right updates) = eitherUpdates
-  let lastUpdateId =
-        if null updates
-          then Nothing
-          else Just (uUpdateId (last updates) + 1)
-  let responses = processUpdates config usersDB updates
-  let upToDateDB =
-        if null responses
-          then usersDB
-          else fst $ last responses
-  let responseBodiesIO = mapM snd responses
-  responseBodies <- responseBodiesIO
-  mapM_ (maybe (BC.putStrLn "map updated") BC.putStrLn) responseBodies
-  longPolling config upToDateDB lastUpdateId
+sendMessage' :: Handle -> ReceiverId -> T.Text -> Query -> IO (Response BC.ByteString)
+sendMessage' botH usrId method query = httpBS (buildRequest host (TE.encodeUtf8 path) finalQuery)
+  where
+    apiKey = (cToken . hConfig) botH
+    path = mconcat ["/bot", apiKey, method]
+    senderId = (BC.pack . show) usrId
+    chatId = ("chat_id", Just senderId)
+    finalQuery = chatId : query
+
+sendKeyboardMessage' :: Handle -> ReceiverId -> IO (Response BC.ByteString)
+sendKeyboardMessage' botH usrId = sendMessage' botH usrId "/sendMessage" query
+  where
+    repeatText = (cRepeatMessage . hConfig) botH
+    query = [("text", Just (TE.encodeUtf8 repeatText)), ("reply_markup", Just (LC.toStrict (A.encode replyKeyboard)))]
+
+sendTextMessage' :: Handle -> T.Text -> ReceiverId -> IO (Response BC.ByteString)
+sendTextMessage' botH message usrId = sendMessage' botH usrId "/sendMessage" query
+  where
+    query = [("text", Just (TE.encodeUtf8 message))]
+
+sendHelpMessage :: Handle -> ReceiverId -> IO (Response BC.ByteString)
+sendHelpMessage botH = sendTextMessage' botH helpText
+  where
+    helpText = (cHelpMessage . hConfig) botH
+
+sendFailMessage :: Handle -> ReceiverId -> IO (Response BC.ByteString)
+sendFailMessage botH = sendTextMessage' botH failText
+  where
+    failText = (cFailMessage . hConfig) botH
+
+sendSticker' :: Handle -> T.Text -> ReceiverId -> IO (Response BC.ByteString)
+sendSticker' botH fileId usrId = sendMessage' botH usrId "/sendSticker" query
+  where
+    query = [("sticker", Just (TE.encodeUtf8 fileId))]
+
+replyMessage' ::
+     Handle -> ReceiverId -> (ReceiverId -> IO (Response BC.ByteString)) -> StateT BotState IO [Response BC.ByteString]
+replyMessage' botH usrId sendFunction = do
+  st <- get
+  let defNoReps = (cNumberOfResponses . hConfig) botH
+  case Map.lookup usrId (sUsers st) of
+    (Just noReps) -> liftIO $ replicateM noReps (sendFunction usrId)
+    Nothing       -> liftIO $ replicateM defNoReps (sendFunction usrId)
+
+processMessage' :: Handle -> Message -> StateT BotState IO ()
+processMessage' botH (TextMessage us txt) = do
+  let usrId = uId us
+  case txt of
+    "/help"   -> void $ liftIO $ sendHelpMessage botH usrId
+    "/repeat" -> void $ liftIO $ sendKeyboardMessage' botH usrId
+    txt'      -> void $ replyMessage' botH usrId (sendTextMessage' botH txt')
+  return ()
+processMessage' botH (StickerMessage us st) =
+  let usrId = uId us
+      fileId = sFileId st
+   in void $ replyMessage' botH usrId (sendSticker' botH fileId)
+processMessage' botH (UnsupportedMessage us) =
+  let usrId = uId us
+   in liftIO $ void (sendFailMessage botH usrId)
+
+processUpdates' :: Config -> MapUserToRepeat -> [Update] -> [(MapUserToRepeat, IO (Maybe BC.ByteString))]
+processUpdates' config usersDB ((UpdateWithMessage _ msg):others) =
+  (usersDB, Just <$> processMessage config usersDB msg) : processUpdates config usersDB others
+processUpdates' config usersDB ((UpdateWithCallback _ callback):others) =
+  (processCallback usersDB callback, pure Nothing) : processUpdates config usersDB others
+processUpdates' config usersDB [] = []
+
+processCallback' :: Handle -> CallbackQuery -> StateT BotState IO ()
+processCallback' botH (CallbackQuery (User usrId) reps) = do
+  st <- get
+  let usersToReps = sUsers st
+  let newMap = Map.insert usrId (read reps) usersToReps
+  let newState = st {sUsers = newMap}
+  put newState
+
+processUpdate :: Handle -> Update -> StateT BotState IO ()
+processUpdate botH (UpdateWithMessage _ msg) = processMessage' botH msg
+processUpdate botH (UpdateWithCallback _ cb) = processCallback' botH cb
+
+--longPolling' :: Handle -> BotM () -> IO ()
+longPolling' :: Handle -> StateT BotState IO ()
+longPolling' botH =
+  forever $ do
+    st <- get
+    let apiKey = (cToken . hConfig) botH
+    let offset = sOffset st
+    eitherUpdates <- liftIO $ getUpdates apiKey offset
+    case eitherUpdates of
+      (Right updates) -> do
+        unless (null updates) (put st {sOffset = Just (uUpdateId (last updates) + 1)})
+        mapM_ (processUpdate botH) updates
+      (Left err) -> undefined
